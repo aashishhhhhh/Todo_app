dccfcbbe17698a0f0ed6bc90e9239743d7fa8125
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2021-03-18 14:44:35 +0545

    First Todo App

--- app/src/main/java/com/example/android_todo_firstt/TodoActivity.java
+++ app/src/main/java/com/example/android_todo_firstt/TodoActivity.java
@@ -1,14 +1,92 @@
 package com.example.android_todo_firstt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TodoActivity extends AppCompatActivity {
 
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+    private static final String TODO_INDEX = "todoIndex";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
+      /* call the super class onCreate to complete the creation
+         of activity with state changes */
         super.onCreate(savedInstanceState);
+
+        // set the user interface layout for this Activity
         setContentView(R.layout.activity_todo);
+
+        // initialize member TextView so we can manipulate it later
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        // read the todo array from res/values/strings.xml
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todos);
+
+        // display the first task from mTodo array in the TodoTextView
+        TodoTextView.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext;
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+        Button buttonPrev;
+        buttonPrev = (Button) findViewById(R.id.buttonPrev);
+
+
+
+
+        // OnClick listener for the  Next button
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                TodoTextView.setText(mTodos[mTodoIndex]);
+            }
+        });
+
+
+
+        buttonPrev.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+
+
+                mTodoIndex = (mTodoIndex - 1) % mTodos.length;
+
+
+                if(mTodoIndex<0)
+                {
+                    mTodoIndex = mTodos.length-1;
+                }
+
+                TodoTextView.setText(mTodos[mTodoIndex]);
+
+
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+
+
     }
 }
